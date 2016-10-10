9334d251fe26a725c8b31fb4b661725eb3197734
lliu05 <jia988727@hotmail.com>
2016-10-10 00:16:43 -0700

    edit html css js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $.ajax({
-      url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote&lang=en',
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=parseQuote',
       type: 'GET',
       contentType: 'application/json',
       dataType: 'jsonp'
@@ -8,7 +8,7 @@ $(document).ready(function() {
       
   $(".nq-btn").click(function() {
       $.ajax({
-      url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote&lang=en',
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=parseQuote',
       type: 'GET',
       contentType: 'application/json',
       dataType: 'jsonp'
@@ -18,8 +18,14 @@ $(document).ready(function() {
 
 function parseQuote(response)
 {
-    $(".quote-1").text((response).quoteText);
-    $(".quote-1").append('<br>');
-    $(".quote-1").append('<br>');
-    $(".quote-1").append((response).quoteAuthor);
+    response = response[0];
+    var node = document.getElementById('quote-id');
+    node.insertAdjacentHTML('beforeend', (response).content);
+    node.insertAdjacentHTML('beforeend', '<br>');
+    node.insertAdjacentHTML('beforeend', '<br>');
+    node.insertAdjacentHTML('beforeend', '<p>'+(response).title+'</p>');
+    //$(".quote-1").text((response).content);
+    //$(".quote-1").append('<br>');
+    //$(".quote-1").append('<br>');
+    //$(".quote-1").append((response).title);
 }
