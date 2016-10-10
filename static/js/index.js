e4b43b8240d204bc094397014dc08877b30a393a
lliu05 <jia988727@hotmail.com>
2016-10-10 00:37:52 -0700

    edit js

--- static/js/index.js
+++ static/js/index.js
@@ -18,14 +18,11 @@ $(document).ready(function() {
 
 function parseQuote(response)
 {
+    $(".quote-content").text("");
     response = response[0];
     var node = document.getElementById('quote-id');
     node.insertAdjacentHTML('beforeend', (response).content);
     node.insertAdjacentHTML('beforeend', '<br>');
     node.insertAdjacentHTML('beforeend', '<br>');
     node.insertAdjacentHTML('beforeend', '<p>'+(response).title+'</p>');
-    //$(".quote-1").text((response).content);
-    //$(".quote-1").append('<br>');
-    //$(".quote-1").append('<br>');
-    //$(".quote-1").append((response).title);
 }
